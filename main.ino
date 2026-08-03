95b31114cc01d680fab60f0c91fb6f4eef7f8e3a
Tiger0-o <145256422+Tiger0-o@users.noreply.github.com>
2026-08-03 10:29:43 +1000

    Add files via upload

--- main.ino
+++ main.ino
@@ -11,7 +11,8 @@
 
 int varPin = A5;
 int buzzerPin = 3;
-int varVar = 0;
+int varChange = 0;
+int ledPins[] = {D8, D9, D10, D11, D12, D13};
 
 void setup () {
     pinMode(A0, INPUT_PULLUP);
@@ -22,27 +23,44 @@ void setup () {
     pinMode(4, INPUT_PULLUP);
     pinMode(5, INPUT_PULLUP);
     pinMode(6, INPUT_PULLUP);
-    
+
+    for (int i = 0; i < 6; i++) {
+        pinMode(ledPins[i], OUTPUT);
+    }
     pinMode(buzzerPin, OUTPUT);
     pinMode(varPin, INPUT);
+    Serial.begin(9600);
 }
 
 void loop() {
+    varChange = analogRead(varPin);
     if (digitalRead(A0) == LOW) {
-        tone(buzzerPin, C4 + varVar, 100);
+        tone(buzzerPin, C4 + varChange, 100);
+        digitalWrite(ledPins[0], HIGH);
     } else if (digitalRead(A1) == LOW) {
-        tone(buzzerPin, D4 + varVar, 100);
+        tone(buzzerPin, D4 + varChange, 100);
+        digitalWrite(ledPins[1], HIGH);
     } else if (digitalRead(A2) == LOW) {
-        tone(buzzerPin, E4 + varVar, 100);
+        tone(buzzerPin, E4 + varChange, 100);
+        digitalWrite(ledPins[2], HIGH);
     } else if (digitalRead(A3) == LOW) {
-        tone(buzzerPin, F4 + varVar, 100);
+        tone(buzzerPin, F4 + varChange, 100);
+        digitalWrite(ledPins[3], HIGH);
     } else if (digitalRead(2) == LOW) {
-        tone(buzzerPin, G4 + varVar, 100);
+        tone(buzzerPin, G4 + varChange, 100);
+        digitalWrite(ledPins[4], HIGH);
     } else if (digitalRead(4) == LOW) {
-        tone(buzzerPin, A4 + varVar, 100);
+        tone(buzzerPin, A4 + varChange, 100);
+        digitalWrite(ledPins[5], HIGH);
     } else if (digitalRead(5) == LOW) {
-        tone(buzzerPin, B4 + varVar, 100);
+        tone(buzzerPin, B4 + varChange, 100);
+        digitalWrite(ledPins[6], HIGH);
     } else if (digitalRead(6) == LOW) {
-        tone(buzzerPin, C5 + varVar, 100);
+        tone(buzzerPin, C5 + varChange, 100);
+        digitalWrite(ledPins[7], HIGH);
+    }
+    delay(10)
+    for (int i = 0; i < 6; i++) {
+        digitalWrite(ledPins[i], LOW);
     }
 }
